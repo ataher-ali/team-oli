f8d4440f2b97fb0600594c8363268937da6bee62
ataher-ali <ataher.dev@gmail.com>
2024-04-25 23:22:36 +0600

    route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,65 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Home from './Pages/Home/Home.jsx';
+import About from './Pages/About/About.jsx';
+import Contact from './Pages/Contact/Contact.jsx';
+import Team from './Pages/Team/Team.jsx';
+import Services from './Pages/Services/Services.jsx';
+import Pricing from './Pages/Pricing/Pricing.jsx';
+import Portfolio from './Pages/Portfolio/Portfolio.jsx';
+import Faq from './Pages/FAQ/Faq.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children:[
+      {
+        path:"/",
+        element: <Home></Home>
+      },
+      {
+        path:"/About",
+        element: <About></About>
+      },
+      {
+        path:"/Contact",
+        element: <Contact></Contact>
+      },
+      {
+        path:"/Team",
+        element: <Team></Team>
+      },
+      {
+        path:"/Services",
+        element: <Services></Services>
+      },
+      {
+        path:"/Pricing",
+        element: <Pricing></Pricing>
+      },
+      {
+        path:"/Portfolio",
+        element: <Portfolio></Portfolio>
+      },
+      {
+        path:"/FAQ",
+        element: <Faq></Faq>
+      },
+
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
