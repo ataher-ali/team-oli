5c01769cc0501c8fcc166bd6d0ec6423d6367f87
ataher-ali <ataher.dev@gmail.com>
2024-04-26 18:17:26 +0600

    gallery

---
+++ src/Components/Home Compo/HomeGallery.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomeGallery = () => {
+    return (
+        <div className='sm:mx-4'>
+            <Link target='_blank' to="/">
+
+
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="" />
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="" />
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="" />
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="" />
+                        </div>
+                    </div>
+
+                </div>
+
+            </Link>
+
+            <div className=' w-100 flex justify-center'>
+                        <Link className=" my-3 md:my-10 group relative inline-block focus:outline-none focus:ring" to="/About">
+                            <span
+                                className="absolute inset-0 translate-x-0 translate-y-0 rounded-lg bg-yellow-300 transition-transform group-hover:translate-x-1.5 group-hover:translate-y-1.5"
+                            ></span>
+
+                            <span
+                                className="relative inline-block border-2 border-current rounded-lg px-8 py-3 text-sm font-bold uppercase tracking-widest"
+                            >
+                                 More
+                            </span>
+                        </Link>
+                    </div>
+        </div>
+    );
+};
+
+export default HomeGallery;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import HomeTestimonial from '../../Components/Home Compo/HomeTestimonial';
 import HomeAbout from '../../Components/Home Compo/HomeAbout';
 import HomeServices from '../../Components/Home Compo/HomeServices';
 import DocumentTitle from 'react-document-title';
+import HomeGallery from '../../Components/Home Compo/HomeGallery';
 
 const Home = () => {
     return (
@@ -16,6 +17,7 @@ const Home = () => {
             <HomeServices></HomeServices>
             <div className='lg:mx-40 md:mx-24'>
                 <HomeAbout></HomeAbout>
+                <HomeGallery></HomeGallery>
             </div>
             <HomeTestimonial></HomeTestimonial>
         </div>
